e53fd7498ea1cb07e274fb780f8027ce8e75b50f
= <=>
2025-07-09 15:15:35 +0530

    Strings Problems

---
+++ Strings/anagram.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<" Enter x : "<<endl;
+    cin>>x;
+    cout<<" Enter y : "<<endl;
+    cin>>y;
+    cout<<" The Sum of "<<x<<" and "<<y<<"is : "<<x+y<<endl;
+    return 0;
+}
